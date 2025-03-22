b5a62cc6ed22ea96b1789fd0562d03ea18b1d755
CoolShivas <shivas2710cool00@gmail.com>
2025-03-22 16:52:03 +0530

    Use of https to get the data from the server.

--- geneJoke/projectJoke.js
+++ geneJoke/projectJoke.js
@@ -1,1 +1,26 @@
- console.log("Joke Generator App");
+import https from "https";
+
+
+const getJokes = () => {
+    const url = `https://official-joke-api.appspot.com/random_joke`;
+
+    https.get(url, (response) => {
+        let data = "";
+        response.addListener('data', (chunk) => {
+            data += chunk;
+        });
+        response.on("end", () => {
+            const joking = JSON.parse(data);
+            console.log(joking);
+            // // Here, we are getting the Output on Terminal as :- 
+            // // {
+            // //     type: 'general',
+            // //     setup: "did you know the first French fries weren't cooked in France?",
+            // //     punchline: 'they were cooked in Greece',
+            // //     id: 31
+            // //   }
+        });
+    })
+};
+
+getJokes();
